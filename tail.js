ec95f0e6dd81228119176cf803c79a2571576bdf
Toktam <ttmohebbi@gmail.com>
2020-08-19 16:54:22 +0000

    finalized tail.js

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+/*
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); */
