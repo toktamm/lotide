134ce7f293fbbaa1f43b364c7c228c2250765cac
Toktam <ttmohebbi@gmail.com>
2020-08-19 17:26:07 +0000

    finished eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+const eqArrays = (a1, a2) => {
+  
+  //handle arraylengths
+  if (a1.length !== a2.length) {
+    console.log("Arrays being compared must be of same length.");
+    return;
+  }
+
+  for (let i = 0; i < a1.length; i++) {
+    if (a1[i] !== a2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
