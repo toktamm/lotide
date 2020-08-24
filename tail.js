4059381b4e2fe5aef308f78a4887d6074c8d2dcb
Toktam <ttmohebbi@gmail.com>
2020-08-24 19:50:26 +0000

    refactored tail.js

--- tail.js
+++ tail.js
@@ -1,24 +1,7 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴 Assertion failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual.js')
 
 const tail = function(array) {
   return array.slice(1);
 };
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-
-assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
-
-/*
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); */
+module. exports = tail;
---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const assertEqual = require('../assertEqual.js')
+const tail = require('../tail.js')
+
+
+// TEST CODE
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
