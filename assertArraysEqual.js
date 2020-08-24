badf026eff20ddee729236e22e986e2796e75e0f
Toktam <ttmohebbi@gmail.com>
2020-08-24 20:26:29 +0000

    refactored assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,15 +1,4 @@
-const eqArrays = (a1, a2) => {
-  if (a1.length !== a2.length) {
-    console.log("Arrays being compared must be of same length.");
-    return;
-  }
-  for (let i = 0; i < a1.length; i++) {
-    if (a1[i] !== a2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays')
 
 
 const assertArraysEqual = (a1, a2) => {
@@ -17,3 +6,4 @@ const assertArraysEqual = (a1, a2) => {
 };
 
 
+module.exports = assertArraysEqual;
